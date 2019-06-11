0eef9c2898e99c3ce08bf1a62ab71185b8eccb6d
andriTinas <andrianustinas.siskom@gmail.com>
2019-06-11 16:26:29 +0700

    v0.6

--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,25 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('/ElaAdmin/', 'ElaAdmin.pages.home');
+Route::view('/ElaAdmin/forms-basic', 'ElaAdmin.forms.pages.basic');
+Route::view('/ElaAdmin/forms-advanced', 'ElaAdmin.forms.pages.advanced');
+Route::view('/ElaAdmin/tables-basic', 'ElaAdmin.tables.pages.basic');
+Route::view('/ElaAdmin/tables-data', 'ElaAdmin.tables.pages.data');
+Route::view('/ElaAdmin/maps-gmap', 'ElaAdmin.maps.pages.gmap');
+Route::view('/ElaAdmin/maps-vector', 'ElaAdmin.maps.pages.vector');
+Route::view('/ElaAdmin/font-fontawesome', 'ElaAdmin.font.pages.fontawesome');
+Route::view('/ElaAdmin/font-themify', 'ElaAdmin.font.pages.themify');
+Route::view('/ElaAdmin/widgets', 'ElaAdmin.widgets.pages.widgets');
 
-Route::view('/', 'ElaAdmin.pages.home');
-Route::view('/forms-basic', 'ElaAdmin.forms.pages.basic');
-Route::view('/forms-advanced', 'ElaAdmin.forms.pages.advanced');
-Route::view('/tables-basic', 'ElaAdmin.tables.pages.basic');
-Route::view('/tables-data', 'ElaAdmin.tables.pages.data');
-Route::view('/maps-gmap', 'ElaAdmin.maps.pages.gmap');
-Route::view('/maps-vector', 'ElaAdmin.maps.pages.vector');
-Route::view('/font-fontawesome', 'ElaAdmin.font.pages.fontawesome');
-Route::view('/font-themify', 'ElaAdmin.font.pages.themify');
-Route::view('/widgets', 'ElaAdmin.widgets.pages.widgets');
-
-Route::get('/ui-{ui}', function($ui){
+Route::get('/ElaAdmin/ui-{ui}', function($ui){
     return view()->exists("ElaAdmin.ui.pages.$ui") ? view("ElaAdmin.ui.pages.$ui") : abort(404);
 });
-Route::get('/charts-{chart}', function($chart){
+Route::get('/ElaAdmin/charts-{chart}', function($chart){
     return view()->exists("ElaAdmin.charts.pages.$chart") ? view("ElaAdmin.charts.pages.$chart") : abort(404);
 });
 
-Route::get('/auth-{auth}', function($auth){
+Route::get('/ElaAdmin/auth-{auth}', function($auth){
     return view()->exists("ElaAdmin.auth.pages.$auth") ? view("ElaAdmin.auth.pages.$auth") : abort(404);
 });
+
